b27fac7e5a307657a6d6e6b595e20a03619f90e2
volodymyrdotsenko <94190791+volodymyrdotsenko@users.noreply.github.com>
2023-10-06 15:49:23 +0300

    m

--- js/main.js
+++ js/main.js
@@ -129,6 +129,13 @@
 	    	}
 		});
 
+	// 	  $(".client-lists").owlCarousel({
+    //   items: 3,  // Number of items to display
+    //   loop: true,  // Enable loop
+    //   nav: true,  // Enable navigation arrows
+    //   navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"]  // Custom navigation arrow icons
+    // });
+
 	};
   	
 
@@ -343,40 +350,6 @@
 
 // 		});
    // };	
-
-
-  /* AjaxChimp
-	* ------------------------------------------------------ */
-	var ssAjaxChimp = function() {
-
-		$('#mc-form').ajaxChimp({
-			language: 'es',
-		   url: cfg.mailChimpURL
-		});
-
-		// Mailchimp translation
-		//
-		//  Defaults:
-		//	 'submit': 'Submitting...',
-		//  0: 'We have sent you a confirmation email',
-		//  1: 'Please enter a value',
-		//  2: 'An email address must contain a single @',
-		//  3: 'The domain portion of the email address is invalid (the portion after the @: )',
-		//  4: 'The username portion of the email address is invalid (the portion before the @: )',
-		//  5: 'This email address looks fake or invalid. Please enter a real email address'
-
-		$.ajaxChimp.translations.es = {
-		  'submit': 'Submitting...',
-		  0: '<i class="fa fa-check"></i> We have sent you a confirmation email',
-		  1: '<i class="fa fa-warning"></i> You must enter a valid e-mail address.',
-		  2: '<i class="fa fa-warning"></i> E-mail address is not valid.',
-		  3: '<i class="fa fa-warning"></i> E-mail address is not valid.',
-		  4: '<i class="fa fa-warning"></i> E-mail address is not valid.',
-		  5: '<i class="fa fa-warning"></i> E-mail address is not valid.'
-		} 
-
-	};
-
  
   /* Back to Top
 	* ------------------------------------------------------ */
@@ -417,8 +390,8 @@
 		ssAlertBoxes();
 		ssAnimations();
 		ssIntroAnimation();		
-		ssContactForm();
-		ssAjaxChimp();
+		// ssContactForm();
+		//ssAjaxChimp();
 		ssBackToTop();
 
 	})();
